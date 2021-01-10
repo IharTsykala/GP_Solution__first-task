3e5e96c4a8a9d8c0c442da948a4b1acf9e599763
IharTsykala <Sudja09@bk.ru>
2021-01-11 01:44:47 +0300

    Updates

---
+++ precache-manifest.a5fe2592d09bfb36c68ba7cb9fb7666b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea3d380bddbd75d950bfcd829d979fd",
+    "url": "/GP_Solution__first-task/index.html"
+  },
+  {
+    "revision": "7bbe79573484f9edd74b",
+    "url": "/GP_Solution__first-task/static/css/main.11a3285b.chunk.css"
+  },
+  {
+    "revision": "e99beb172f5edd5d6e31",
+    "url": "/GP_Solution__first-task/static/js/2.28c4b31e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/GP_Solution__first-task/static/js/2.28c4b31e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbe79573484f9edd74b",
+    "url": "/GP_Solution__first-task/static/js/main.195ada26.chunk.js"
+  },
+  {
+    "revision": "061c95d8697232b13b4f",
+    "url": "/GP_Solution__first-task/static/js/runtime-main.e90318c5.js"
+  }
+]);
