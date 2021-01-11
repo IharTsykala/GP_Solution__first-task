6744dc456fe764dd1b29cb7a64708e61e8b9361c
IharTsykala <Sudja09@bk.ru>
2021-01-11 03:36:16 +0300

    ref: refactor formik

--- src/Components/NewsForm/NewsFormRender/NewsFormUseStyles.tsx
+++ src/Components/NewsForm/NewsFormRender/NewsFormUseStyles.tsx
@@ -4,8 +4,10 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       "& > *": {
-        margin: theme.spacing(1),
-        width: "25ch",
+        width: "30vw",
+        display: "grid",
+        height: "8vh",
+        margin: "0 auto",
       },
     },
   })
